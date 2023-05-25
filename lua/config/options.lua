70fb9f97344006778bd159de39461fd660ff2347
MyArch.ins <mr.dongsuibian@gmail.com>
2023-05-25 20:08:38 +0800

    init nvim

---
+++ lua/config/options.lua
@@ -0,0 +1,15 @@
+--
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local opt = vim.opt
+opt.relativenumber = true --relatvive number
+opt.number=true -- number
+opt.cursorline = true -- Enable highlighting of the current line
+opt.cursorcolumn = true -- Enable highlighting of the current column
+opt.clipboard = "unnamedplus" -- Sync with system clipboard
+opt.tabstop = 4 --number of spaces tabs count for
+opt.expandtab = true --use spaces instead of tabs
+
+
+
