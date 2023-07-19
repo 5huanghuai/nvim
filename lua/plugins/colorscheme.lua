fc3da1456e5056623b838245e839cf8d27b11d02
MyArch.ins <mr.dongsuibian@gmail.com>
2023-07-19 17:05:37 +0800

    set ui
    对nvim的ui进行了基本的设置

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -22,4 +22,232 @@ config = function(_,opts)
 end,
 },                           
 
+            
+--  +------------------------------------------------------------------------------+
+--  |                           Beautifull notification                            |
+--  +------------------------------------------------------------------------------+
+    {
+    "rcarriga/nvim-notify",
+    keys = {
+      { "<leader>nD", function() require("notify").dismiss({ silent = true, pending = true }) end, desc = "Delete all Notifications", },
+    },
+    opts = {
+      timeout = 1000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+    },
+  },
+
+  -- noicer ui
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+      dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+    },
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = true,
+        lsp_doc_border = true,
+      },
+    },
+    keys = {
+      { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline", },
+      { "<leader>nt", function() require("noice").cmd("telescope") end, desc = "Noice Telescope", },
+      { "<leader>nl", function() require("noice").cmd("last") end, desc = "Noice Last Message", },
+      { "<leader>nh", function() require("noice").cmd("history") end, desc = "Noice History", },
+      { "<leader>na", function() require("noice").cmd("all") end, desc = "Noice All", },
+      { "<leader>nd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+      {
+        "<c-f>",
+        function()
+          if not require("noice.lsp").scroll(4) then
+            return "<c-f>"
+          end
+        end,
+        silent = true,
+        expr = true,
+        desc = "Scroll forward",
+        mode = { "i", "n", "s" },
+      },
+      {
+        "<c-b>",
+        function()
+          if not require("noice.lsp").scroll(-4) then
+            return "<c-b>"
+          end
+        end,
+        silent = true,
+        expr = true,
+        desc = "Scroll backward",
+        mode = { "i", "n", "s" },
+      },
+    },
+  },
+
+
+    -- make theme transparent
+    {
+        "xiyaowong/transparent.nvim",
+        keys = {
+            { "<leader>tt", "<cmd>TransparentToggle<cr>", mode = { "i", "n", "s" },desc = "TransparentToggle" },
+        },
+        config =function()
+        
+        require("transparent").setup({
+          groups = { -- table: default groups
+            'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+            'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+            'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+            'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+          },
+          extra_groups = {
+            "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+            "NvimTreeNormal" -- NvimTree
+          }, -- table: additional groups that should be cleared
+          exclude_groups = {}, -- table: groups you don't want to clear
+        })
+        end
+    },
+
+
+	-- theme
+	{
+		"RRethy/nvim-base16",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- color scheme
+			--vim.cmd.colorscheme("base16-tender")
+			--vim.cmd.colorscheme("base16-gruvbox-dark-soft")
+			--vim.cmd.colorscheme("base16-gruvbox-dark-medium")
+			vim.cmd.colorscheme("base16-gruvbox-dark-pale")
+			--vim.cmd.colorscheme("base16-gruvbox-material-dark-hard")
+			--vim.cmd.colorscheme("base16-gruvbox-material-dark-medium")
+			--vim.cmd.colorscheme("base16-gruvbox-material-dark-soft")
+		end,
+	},
+
+	--status line
+	{
+		"ojroques/nvim-hardline",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("hardline").setup({
+				bufferline = true, -- disable bufferline
+				bufferline_settings = {
+					exclude_terminal = false, -- don't show terminal buffers in bufferline
+					show_index = false, -- show buffer indexes (not the actual buffer numbers) in bufferline
+				},
+				theme = "gruvbox", -- change theme
+				sections = { -- define sections
+					{ class = "mode", item = require("hardline.parts.mode").get_item },
+					-- , hide = 100
+					{ class = "branch", item = require("hardline.parts.git").get_item },
+					{ class = "med", item = require("hardline.parts.filename").get_item },
+					-- hide = 60
+					"%<",
+					{ class = "med", item = "%=" },
+					{ class = "high", item = require("hardline.parts.filetype").get_item },
+
+					-- { class = "low", item = require("hardline.parts.wordcount").get_item, hide = 100 },
+					{ class = "error", item = require("hardline.parts.lsp").get_error },
+					{ class = "warning", item = require("hardline.parts.lsp").get_warning },
+					--{ class = "med", item = require("hardline.parts.whitespace").get_item },
+					{ class = "med", item = require("hardline.parts.line").get_item },
+					{
+						class = "low",
+						item = function()
+							return " " .. os.date("%R")
+						end,
+					},
+				},
+			})
+		end,
+	},
+
+
+--  +------------------------------------------------------------------------------+
+  --  |                                  Dashboard                                   |
+  --  +------------------------------------------------------------------------------+
+  {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    opts = function()
+      local dashboard = require("alpha.themes.dashboard")
+      local logo = require("util.logo")["random"]
+      dashboard.section.header.val = vim.split(logo, "\n")
+      dashboard.section.buttons.val = {
+        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+        dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+        -- dashboard.button("r", " " .. " Recent files", ":Telescope frecency <CR>"),
+        -- dashboard.button("g", " " .. " Grep text", ":Telescope live_grep <CR>"),
+        -- dashboard.button("u", "鈴" .. " Update plugins", ":Lazy update<CR>"),
+        -- dashboard.button("c", " " .. " Config NeoVim", ":e $MYVIMRC <CR>"),
+        dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+      }
+      for _, button in ipairs(dashboard.section.buttons.val) do
+        button.opts.hl = "AlphaButtons"
+        button.opts.hl_shortcut = "AlphaShortcut"
+      end
+      dashboard.section.footer.opts.hl = "Type"
+      dashboard.section.header.opts.hl = "AlphaHeader"
+      dashboard.section.buttons.opts.hl = "AlphaButtons"
+      dashboard.opts.layout[1].val = 8
+      return dashboard
+    end,
+    config = function(_, dashboard)
+      vim.b.miniindentscope_disable = true
+
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "AlphaReady",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+
+      require("alpha").setup(dashboard.opts)
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "LazyVimStarted",
+        callback = function()
+          local stats = require("lazy").stats()
+          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+
+          local version = " v" .. vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch
+          local plugins = "⚡Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+          local footer = "\t" .. version .. "\t" .. plugins
+          dashboard.section.footer.val = footer
+          pcall(vim.cmd.AlphaRedraw)
+        end,
+      })
+    end,
+  },
+
+
 }
